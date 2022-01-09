eb554c313a2d49b024a2e80d75802e53c40b6acc
MDarowny <michal.darowny@gmail.com>
2022-01-09 02:51:12 +0100

    removed print

--- users/controllers/user.go
+++ users/controllers/user.go
@@ -2,7 +2,6 @@ package controllers
 
 import (
 	"database/sql"
-	"fmt"
 	"github.com/labstack/echo/v4"
 	"net/http"
 )
@@ -12,22 +11,14 @@ type User struct {
 }
 
 func (u *User) Hello(c echo.Context) error {
-	fmt.Println(u.repository)
-	fmt.Println(u)
-
 	return c.String(http.StatusOK, "Hello, World!")
 }
 
 func (u *User) GetOne(c echo.Context) error {
 	id := c.Param("id")
-	fmt.Println(u.repository)
-	fmt.Println(u)
-
 	return c.String(http.StatusOK, id)
 }
 
 func (u *User) Register(c echo.Context) error {
-	fmt.Println(u.repository)
-	fmt.Println(u)
 	return c.String(http.StatusOK, "REGISTERED")
 }
